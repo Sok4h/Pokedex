e6f797aa5514c12d83d22d35231cee95d006c585
Sok4h <mrsokaxd@gmail.com>
2022-04-30 21:59:16 -0500

    added input with pokeball Icon

--- app/src/main/java/com/sokah/pokedex/MainActivity.kt
+++ app/src/main/java/com/sokah/pokedex/MainActivity.kt
@@ -2,10 +2,19 @@ package com.sokah.pokedex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val input = findViewById<TextInputLayout>(R.id.inputPokemon)
+
+        input.setEndIconOnClickListener {
+
+            Toast.makeText(this,"Buscando pokemon",Toast.LENGTH_SHORT).show()
+        }
     }
 }
